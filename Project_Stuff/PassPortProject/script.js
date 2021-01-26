360bc8dd9ce21072f1de4249d04b9e2bc0af2448
Zachary Taylor <Ztaylo273@west-mec.local>
2021-01-26 15:20:02 -0700

    Did alot of Java like Finshed the inputs

--- Project_Stuff/PassPortProject/script.js
+++ Project_Stuff/PassPortProject/script.js
@@ -1,6 +1,7 @@
-let firstName = "Nothing"
+
+let passPortID = 10000;
 class UserBasicInfo{
-    constructor(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber){
+    constructor(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID){
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
@@ -9,35 +10,60 @@ class UserBasicInfo{
         this.startDate = startDate;
         this.endDate = endDate;
         this.bagNumber = bagNumber;
-    }
-}
-
-class UserWithMeal extends UserBasicInfo{
-    constructor(Meal){
-        super();
         this.Meal = Meal;
-    }
-}
-class UserWithExtra extends UserBasicInfo{
-    constructor(Extra){
-        super();
         this.Extras = Extra;
+        this.passPortID = passPortID;
     }
 }
-function submit(){
-
-}
-
-
-let boi = new UserBasicInfo("Jimmy", "Pie" , new Date("August 8, 2003"), "Arizona", "Flordia", new Date("August 26, 2020"), new Date("August 29, 2021"),10) 
-let boi = boi + new UserWithMeal("Pizza")
-console.log(`Last name is ${boi.lastName}`)
-console.log(`First name is ${boi.firstName}`)
-console.log(`Birth Date ${boi.dateOfBirth}`)
-console.log(`Start Place ${boi.startPlace}`)
-console.log(`Where they are going ${boi.endPlace}`)
-console.log(`The date they are leaving ${boi.startDate}`)
-console.log(`The date they are coming back ${boi.endDate}`)
-console.log(`The bags they have ${boi.bagNumber}`)
-console.log(`The meal he has chosen ${boi.Meal}`)
-console.log(`The Extra he has chosen ${boi.Extra}`)
+let userList =[]
+function addToList(){
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let dateOfBirth = document.getElementById("DoB").value;
+    let startPlace = document.getElementById("departing").value;
+    let endPlace = document.getElementById("arriving").value;
+    let startDate = document.getElementById("leaveDate").value;
+    let endDate = document.getElementById("returnDate").value;
+    let bagNumber = document.getElementById("bags").value;
+    let Meal = document.querySelector(`input[name=meal]:checked`).value;
+    let extra = document.getElementsByName("extra");
+    let Extra = ``;
+    let Cost = 0;
+    let bagCost = 0;
+    let extraCost=0;
+    for(i=0;i< extra.length; i++){
+        if(extra[i].checked === true){
+            Extra += extra[i].value + ` `;
+            return(extraCost)
+        }
+    }
+    if(firstName != "" && lastName != "" && dateOfBirth != "" && startPlace != "" && endPlace != "" && startDate != "" && endDate != "" && Meal != "" && Extra != "" && bagNumber != ""){
+        let user = new UserBasicInfo(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID);
+        userList.push(user);
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        extraCost = extra.length*10
+        Cost = bagCost + extraCost
+        console.log(Cost)
+        alert("Its working")
+        console.log(userList)
+        passPortID++
+        return(passPortID)
+    }else if(firstName != "" && lastName != "" && dateOfBirth != "" && startPlace != "" && endPlace != "" && startDate != "" && endDate != "" && Meal != "" && Extra == "" && bagNumber != ""){
+        let user = new UserBasicInfo(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID);
+        userList.push(user);
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        Extra += "nothing";
+        alert("Its working");
+        console.log(userList)
+        bagCost = bagNumber*20
+        Cost = bagCost + extraCost
+        console.log(Cost)
+        passPortID++
+        return(passPortID)
+}else{
+        alert("You missed something")
+        console.log(Extra)
+}}
+let boi = new UserBasicInfo("Jimmy", "Pie" , new Date("August 8, 2003"), "Arizona", "Flordia", new Date("August 26, 2020"), new Date("August 29, 2021"),10)
