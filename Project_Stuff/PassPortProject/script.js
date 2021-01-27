7347f3dee5107cf45bf112bc5589ebb8126e9681
Zachary Taylor <ztaylo273@west-mec.com>
2021-01-26 18:52:53 -0700

    Finshed the base project

--- Project_Stuff/PassPortProject/script.js
+++ Project_Stuff/PassPortProject/script.js
@@ -1,7 +1,7 @@
 
 let passPortID = 10000;
 class UserBasicInfo{
-    constructor(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID){
+    constructor(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID,Cost,age,Drink,time){
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
@@ -13,6 +13,10 @@ class UserBasicInfo{
         this.Meal = Meal;
         this.Extras = Extra;
         this.passPortID = passPortID;
+        this.cost= Cost;
+        this.age= age;
+        this.Beer = Drink;
+        this.time = time;
     }
 }
 let userList =[]
@@ -28,42 +32,68 @@ function addToList(){
     let Meal = document.querySelector(`input[name=meal]:checked`).value;
     let extra = document.getElementsByName("extra");
     let Extra = ``;
-    let Cost = 0;
+
+    let Cost = 300;
     let bagCost = 0;
     let extraCost=0;
+    let numberOfExtras = document.getElementsByName("extra").length;
+    bagCost = bagNumber*20;
+    extraCost = numberOfExtras*10;
+    Cost += bagCost + extraCost;
+
+    let Drink = true;
+    let Birthday = new Date(dateOfBirth);
+    let aBirthday = Birthday.getFullYear();
+    let today = new Date().getFullYear();
+    let age = today - aBirthday;
+    if(age < 21){
+        Drink = false;
+    };
+
+
+    let leaveDate = new Date(startDate);
+    let backDate = new Date(endDate);
+    let leaveDay = leaveDate.getDay()
+    let backday = backDate.getDay()
+    let time = backday - leaveDay;
+
     for(i=0;i< extra.length; i++){
         if(extra[i].checked === true){
             Extra += extra[i].value + ` `;
-            return(extraCost)
         }
     }
     if(firstName != "" && lastName != "" && dateOfBirth != "" && startPlace != "" && endPlace != "" && startDate != "" && endDate != "" && Meal != "" && Extra != "" && bagNumber != ""){
-        let user = new UserBasicInfo(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID);
+        let user = new UserBasicInfo(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID,Cost,age,Drink,time);
         userList.push(user);
         document.getElementById("firstName").value = "";
         document.getElementById("lastName").value = "";
-        extraCost = extra.length*10
-        Cost = bagCost + extraCost
-        console.log(Cost)
         alert("Its working")
         console.log(userList)
         passPortID++
         return(passPortID)
     }else if(firstName != "" && lastName != "" && dateOfBirth != "" && startPlace != "" && endPlace != "" && startDate != "" && endDate != "" && Meal != "" && Extra == "" && bagNumber != ""){
-        let user = new UserBasicInfo(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID);
+        let user = new UserBasicInfo(firstName, lastName, dateOfBirth, startPlace, endPlace,startDate,endDate,bagNumber,Meal,Extra,passPortID,Cost,age,Drink,time);
         userList.push(user);
         document.getElementById("firstName").value = "";
         document.getElementById("lastName").value = "";
         Extra += "nothing";
         alert("Its working");
         console.log(userList)
-        bagCost = bagNumber*20
-        Cost = bagCost + extraCost
-        console.log(Cost)
         passPortID++
         return(passPortID)
 }else{
         alert("You missed something")
         console.log(Extra)
-}}
+}
+}
+
+function print(){
+    let space = document.getElementById("names");
+    space.innerHTML = " ";
+    for(let i = 0; i < userList.length; i++){
+        space.innerHTML += `<div><span>${userList[i].passPortID}</span>${userList[i].firstName} ${userList[i].lastName}</div>`
+    }
+}
+
+console.log("Does the script still work?")
 let boi = new UserBasicInfo("Jimmy", "Pie" , new Date("August 8, 2003"), "Arizona", "Flordia", new Date("August 26, 2020"), new Date("August 29, 2021"),10)
